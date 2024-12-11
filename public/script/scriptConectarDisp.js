e88ac28f248aeb6a596319b2f8aae6f829382edc
marin <marin@STERBLICHEN>
2024-12-10 21:03:10 -0300

    Cambios finales

---
+++ public/script/scriptConectarDisp.js
@@ -0,0 +1,10 @@
+function Atras(){
+    window.location.href = 'index.html';
+}
+
+document.getElementById('Conectar').addEventListener('click',()=>{
+    document.getElementById('LabelConect').textContent = 'Conectado';
+})
+document.getElementById('Desconectar').addEventListener('click',()=>{
+    document.getElementById('LabelConect').textContent = 'Desconectado';
+})
